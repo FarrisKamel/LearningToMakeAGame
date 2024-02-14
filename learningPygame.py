43c2a94192935cdbd3c4dfee58ff2fc361d373de
Farris Kamel <farris.alqalam@gmail.com>
2024-02-14 13:27:15 -0500

    README Created

--- learningPygame.py
+++ learningPygame.py
@@ -2,35 +2,6 @@
 # This is my first attempt to building a game. 
 # After research, it was recommend to use Pygame.
 
-# Game Process - 
-#   1. Checking the player input (the event loop)         <-------|
-#   2. Use that information to place elements in the screen       |
-#   Result = Creates an image                            >--------|
-
-# Pygame - 
-#   1. Helps draws images. 
-#   2. Check for player input 
-#       (input() stops the game, therefore useless of game)
-#   3. Made only for simply game. (Typically 2-D games).
-
-# Deciding Frame rate of the game -
-#   1. Strong PC can handle high frame rate
-#   Ideally - 
-#       - Should use constant framerate
-#       - We will use 60 fps for this 
-#           * 60 fps ceiling
-#           * 60 fps floor 
-
-# Displaying Images -
-#   Using surfaces:
-#       - Display Surface (The game window)
-#           * Can only be one
-#       - Regular Surface (Essential an image)
-#           * Needs to be put in the display surface 
-#           * Can be many 
-
-##############################################
-
 # Creating a windown in Pygame
 import pygame
 from sys import exit
